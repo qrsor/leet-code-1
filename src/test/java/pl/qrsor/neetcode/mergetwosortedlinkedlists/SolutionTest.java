e2c9b17011d9ca19b317c3faf58ef12d0f10f885
qrsor <3127715+qrsor@users.noreply.github.com>
2025-02-10 20:25:39 +0100

    Merge sorted linked lists

---
+++ src/test/java/pl/qrsor/neetcode/mergetwosortedlinkedlists/SolutionTest.java
@@ -0,0 +1,37 @@
+package pl.qrsor.neetcode.mergetwosortedlinkedlists;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+    @Test
+    void shouldMergeLists() {
+        //given
+        var leaf1 = new ListNode(4);
+        var mid1 = new ListNode(2, leaf1);
+        var head1 = new ListNode(1, mid1);
+
+        var leaf2 = new ListNode(5);
+        var mid2 = new ListNode(3, leaf2);
+        var head2 = new ListNode(1, mid2);
+
+        //when
+        var result = new Solution().mergeTwoLists(head1, head2);
+
+
+        //then
+        var values = new ArrayList<Integer>();
+        var node = result;
+        do {
+            values.add(node.val);
+            node = node.next;
+        } while (node != null);
+
+        assertThat(values).containsExactly(1, 1, 2, 3, 4, 5);
+
+    }
+}

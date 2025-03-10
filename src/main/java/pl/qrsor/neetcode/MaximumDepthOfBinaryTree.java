be5352e16743c923f78a0d148a0eca8d796987ce
qrsor <3127715+qrsor@users.noreply.github.com>
2025-03-10 21:15:48 +0100

    Binary tree depth recursive

--- src/main/java/pl/qrsor/neetcode/MaximumDepthOfBinaryTree.java
+++ src/main/java/pl/qrsor/neetcode/MaximumDepthOfBinaryTree.java
@@ -4,32 +4,7 @@ import java.util.Stack;
 
 public class MaximumDepthOfBinaryTree {
     public int maxDepth(TreeNode root) {
-
         if(root==null) return 0;
-
-        int depth = 0;
-        Stack<TreeNode> wq = new Stack<>();
-        Stack<TreeNode> path = new Stack<>();
-
-        wq.push(root);
-
-        TreeNode current;
-        while (!wq.empty()) {
-            current = wq.peek();
-            if (!path.empty() && current == path.peek()) {
-                if (path.size() > depth)
-                    depth = path.size();
-                path.pop();
-                wq.pop();
-            } else {
-                path.push(current);
-                if (current.right != null)
-                    wq.push(current.right);
-                if (current.left != null)
-                    wq.push(current.left);
-            }
-        }
-
-        return depth;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
 }
